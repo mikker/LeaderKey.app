68bbfb127c31a5a52234d21ebad32a35498682ad
Mikkel Malmberg <mikkel@brnbw.com>
2024-04-19 22:55:18 +0200

    Initial Commit

---
+++ Leader Key/Leader_KeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Leader_KeyApp.swift
+//  Leader Key
+//
+//  Created by Mikkel Malmberg on 19/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Leader_KeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
